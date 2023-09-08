e26c410da945b680606c38bb34b7b5cefee183af
Arun Kumar <aruncode49@gmail.com>
2023-09-09 00:17:08 +0530

    jwt complete

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello Server</h1>");
 });
 
+// create login post route for generatin token with user details
 app.post("/login", (req, res) => {
   const user = {
     id: 1,
@@ -24,5 +25,27 @@ app.post("/login", (req, res) => {
     res.json(user);
   });
 });
+
+// Create profile route with a middleware function
+app.post("/profile", verifyToken, (req, res) => {
+  jwt.verify(req.token, secretKey, (err, authData) => {
+    if (err) {
+      res.json({ error: "Token Mismatched" });
+    } else {
+      res.json(authData);
+    }
+  });
+});
+
+// Create a function to verify the token
+function verifyToken(req, res, next) {
+  const authToken = req.headers.auth;
+  if (authToken) {
+    req.token = authToken;
+  } else {
+    res.json({ err: "Invalid User" });
+  }
+  next();
+}
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
